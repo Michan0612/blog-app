4fc538b90b089ead42f486d5523ed766b93fb0d7
Tanaka Mitsuyuki <tomoka6412@gmail.com>
2020-06-13 08:06:53 +0900

    微調整

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,15 @@
-# frozen_string_literal: true
-
 require_relative 'boot'
-
 require 'rails/all'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module BlogApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    Bundler.require(*Rails.groups)
+    Dotenv::Railtie.load
     if Rails.env.development? || Rails.env.test?
       Bundler.require(*Rails.groups)
       Dotenv::Railtie.load
