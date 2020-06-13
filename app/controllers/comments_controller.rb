210182a83a8a9e1bf2dbc15faf96ff5a1892f005
Tanaka Mitsuyuki <tomoka6412@gmail.com>
2020-06-13 21:25:27 +0900

    CommentsControllerを変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,22 @@
-# frozen_string_literal: true
-
 class CommentsController < ApplicationController
-  def new
-    article = Article.find(params[:article_id])
-    @comment = article.comments.build
-  end
-
-  def create
-    article = Article.find(params[:article_id])
-    @comment = article.comments.build(comment_params)
-    if @comment.save
-      redirect_to article_path(article), notice: 'コメントを追加'
-    else
-      flash.now[:error] = '更新できませんでした'
-      render :new
+    def new
+      article = Article.find(params[:article_id])
+      @comment = article.comments.build
     end
-  end
-
-  private
-
-  def comment_params
-    params.require(:comment).permit(:content)
-  end
+  
+    def create
+      article = Article.find(params[:article_id])
+      @comment = article.comments.build(comment_params)
+      if @comment.save
+        redirect_to article_path(article), notice: 'コメントを追加'
+      else
+        flash.now[:error] = '更新できませんでした'
+        render :new
+      end
+    end
+  
+    private
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
 end
